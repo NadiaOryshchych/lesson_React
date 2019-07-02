5344f108ed38718fdcd6ed3f8836d32ff2564941
Nadia <nadia.naka.ne@gmail.com>
2019-07-02 21:23:41 +0300

    add sixteenth lesson

---
+++ lesson-16/resto/src/components/app-header/app-header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import cartIcon from './shopping-cart-solid.svg';
+import './app-header.scss';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+const AppHeader = ({total}) => {
+    return (
+        <header className="header">
+            <Link to='/' className="header__link">
+                Menu
+            </Link>
+            <Link to='/cart-page' className="header__link">
+                <img className="header__cart" src={cartIcon} alt="cart"></img>
+                Total: {total} $
+            </Link>
+        </header>
+    )
+};
+
+const mapStateToProps = (state) => {
+    return {
+        total: state.total
+    }
+};
+
+export default connect(mapStateToProps)(AppHeader);
---
+++ lesson-16/resto/src/reducers/index.js
@@ -0,0 +1,78 @@
+const initialState = {
+  menu: [],
+  items: [],
+  loading: true,
+  error: false,
+  total: 0
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'MENU_LOADED':
+      return {
+        ...state,
+        menu: action.payload,
+        loading: false,
+        error: false
+      };
+    case 'MENU_REQUESTED':
+      return {
+        ...state,
+        menu: state.menu,
+        loading: true,
+        error: false
+      };
+    case 'MENU_ERROR':
+      return {
+        ...state,
+        menu: state.menu,
+        loading: false,
+        error: true
+      };
+    case 'ITEM_ADD_TO_CART':
+      const id = action.payload;
+      const item = state.menu.find(item => item.id === id);
+      const newItem = {
+        title: item.title,
+        price: item.price,
+        url: item.url,
+        id: item.id,
+        counter: 1
+      };
+      const twin = state.items.find(item => item.id === id);
+      if (twin) {
+        twin.counter++;
+        const items = [...state.items];
+        return {
+          ...state,
+          items,
+          total: (state.total + parseInt(newItem.price))
+        }
+      } else {
+        return {
+          ...state,
+          items: [
+            ...state.items,
+            newItem
+          ],
+          total: (state.total + parseInt(newItem.price))
+        }
+      }
+    case 'ITEM_REMOVE_FROM_CART':
+      const idx = action.payload;
+      const itemIndex = state.items.findIndex(item => item.id === idx);
+      const delItem = state.items.find(item => item.id === idx);
+      return {
+        ...state,
+        items: [
+          ...state.items.slice(0, itemIndex),
+          ...state.items.slice(itemIndex + 1)
+        ],
+        total: state.total - (delItem.price * delItem.counter)
+      }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
