05b6e3e34b3bf7f0cad4f95daef0debca3dcfaae
Nadia <nadia.naka.ne@gmail.com>
2019-07-09 11:59:40 +0300

    del double router

--- diploma/src/components/app/app.js
+++ diploma/src/components/app/app.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import {MainPage, CoffeePage, PleasurePage, CoffeeItem} from '../pages';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 
 const App = () => {
     return (
-        <Router>
-            <div className="app">
-                <Switch>
-                    <Route path='/' exact component={MainPage}/>
-                    <Route path='/our-coffee' component={CoffeePage}/>
-                    <Route path='/for-your-pleasure' component={PleasurePage}/>
-                    <Route path='/:address' /*component={CoffeeItem}*/ render={
-                        ({match}) => {
-                            const address = match.params;
-                            return  <CoffeeItem coffeeName={address} />
-                        }
-                    }/>
-                </Switch>
-            </div>
-        </Router>
+        <div className="app">
+            <Switch>
+                <Route path='/' exact component={MainPage}/>
+                <Route path='/our-coffee' component={CoffeePage}/>
+                <Route path='/for-your-pleasure' component={PleasurePage}/>
+                <Route path='/:address' /*component={CoffeeItem}*/ render={
+                    ({match}) => {
+                        const address = match.params;
+                        return  <CoffeeItem coffeeName={address} />
+                    }
+                }/>
+            </Switch>
+        </div>
     )
 }
 
