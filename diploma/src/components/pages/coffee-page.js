7b1c7a9e2fe7adfd3e62c56c18e172004372c1d1
Nadia <nadia.naka.ne@gmail.com>
2019-07-10 02:21:23 +0300

    add finaly-finaly version

--- diploma/src/components/pages/coffee-page.js
+++ diploma/src/components/pages/coffee-page.js
@@ -64,60 +64,39 @@ class CoffeePage extends Component {
 
   renderItem = (buttons, visibleList) => {
     return (
-      <section className="shop">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 offset-2">
-              <img className="shop__girl" src="img/coffee_girl.jpg" alt="girl"/>
-            </div>
-            <div className="col-lg-4">
-              <div className="title">About our beans</div>
-              <img className="beanslogo" src="logo/Beans_logo_dark.svg" alt="Beans logo"/>
-              <div className="shop__text">
-                Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-                <br/><br/>
-                Afraid at highly months do things on at. Situation recommend objection do intention<br/>
-                so questions. <br/>
-                As greatly removed calling pleased improve an. Last ask him cold feel<br/>
-                met spot shy want. Children me laughing we prospect answered followed. At it went<br/>
-                is song that held help face.
-              </div>
-            </div>
+      <>
+        <div className="row">
+          <div className="col-lg-4 offset-2">
+            <form action="#" className="shop__search">
+              <label className="shop__search-label" htmlFor="filter">Looking for</label>
+              <input 
+                id="filter" 
+                type="text" 
+                placeholder="start typing here..." 
+                className="shop__search-input"
+                value={this.state.term}
+                onChange={this.onUpdateSearch}/>
+            </form>
           </div>
-          <div className="line"></div>
-          <div className="row">
-            <div className="col-lg-4 offset-2">
-              <form action="#" className="shop__search">
-                <label className="shop__search-label" htmlFor="filter">Looking for</label>
-                <input 
-                  id="filter" 
-                  type="text" 
-                  placeholder="start typing here..." 
-                  className="shop__search-input"
-                  value={this.state.term}
-                  onChange={this.onUpdateSearch}/>
-              </form>
-            </div>
-            <div className="col-lg-4">
-              <div className="shop__filter">
-                <div className="shop__filter-label">
-                  Or filter
-                </div>
-                <div className="shop__filter-group">
-                  {buttons}
-                </div>
+          <div className="col-lg-4">
+            <div className="shop__filter">
+              <div className="shop__filter-label">
+                Or filter
               </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-10 offset-lg-1">
-              <div className="shop__wrapper">
-                <CoffeeList coffeeList={visibleList}/>
+              <div className="shop__filter-group">
+                {buttons}
               </div>
             </div>
           </div>
         </div>
-      </section>
+        <div className="row">
+          <div className="col-lg-10 offset-lg-1">
+            <div className="shop__wrapper">
+              <CoffeeList coffeeList={visibleList}/>
+            </div>
+          </div>
+        </div>
+      </>
     )
   }
    
@@ -141,7 +120,7 @@ class CoffeePage extends Component {
       )
     });
 
-    const content = error ? <div className="error">Error! Coffee page not here...</div> :
+    const content = error ? <div className="error">Error! Something goes wrong :(</div> :
       loading ? <Spinner/> :
       this.renderItem(buttons, visibleList);
 
@@ -149,7 +128,30 @@ class CoffeePage extends Component {
       <>
         <AppHeader/>
         <Banner classStyle={'banner'} title={'Our Coffee'}/>
-        {content}
+        <section className="shop">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 offset-2">
+                <img className="shop__girl" src="img/coffee_girl.jpg" alt="girl"/>
+              </div>
+              <div className="col-lg-4">
+                <div className="title">About our beans</div>
+                <img className="beanslogo" src="logo/Beans_logo_dark.svg" alt="Beans logo"/>
+                <div className="shop__text">
+                  Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+                  <br/><br/>
+                  Afraid at highly months do things on at. Situation recommend objection do intention<br/>
+                  so questions. <br/>
+                  As greatly removed calling pleased improve an. Last ask him cold feel<br/>
+                  met spot shy want. Children me laughing we prospect answered followed. At it went<br/>
+                  is song that held help face.
+                </div>
+              </div>
+            </div>
+            <div className="line"></div>
+            {content}
+          </div>
+        </section>
         <AppFooter/>
       </>
     )
